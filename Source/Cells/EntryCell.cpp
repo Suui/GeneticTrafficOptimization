a1b0aca1c96bdfb84d4060a0980c3d96b69568f2
Suui <suui.gd@gmail.com>
2015-10-31 00:35:11 +0100

    Cleanup

--- Source/Cells/EntryCell.cpp
+++ Source/Cells/EntryCell.cpp
@@ -1,1 +1,26 @@
 ﻿#include "EntryCell.h"
+
+
+void EntryCell::SetState(CellState state)
+{
+	if (state == Empty && vehicleQueue.size() > 0)
+	{
+		vehicle = vehicleQueue.front();
+		vehicleQueue.pop();
+	}
+	else
+		this->state = Empty;
+}
+
+
+void EntryCell::AddVehicle()
+{ 
+	vehicleQueue.push(std::make_shared<Vehicle>(vehicleID));
+	vehicleID += 1;
+
+	if (IsOccupied()) return;
+
+	direction == Vertical ? this->state = VerticallyOccupied : this->state = HorizontallyOccupied;
+	vehicle = vehicleQueue.front();
+	vehicleQueue.pop();
+}
--- Source/Cells/EntryCell.h
+++ Source/Cells/EntryCell.h
@@ -14,27 +14,8 @@ public:
 	EntryCell(Direction direction) : Cell(direction) {}
 
 	bool isEntryCell() override { return true; }
-	
-	void SetState(CellState state) override 
-	{
-		if (state == Empty && vehicleQueue.size() > 0)
-		{
-			vehicle = vehicleQueue.front();
-			vehicleQueue.pop();
-		}
-		else
-			this->state = Empty;
-	}
-	
-	void AddVehicle() override 
-	{ 
-		vehicleQueue.push(std::make_shared<Vehicle>(vehicleID));
-		vehicleID += 1;
 
-		if (IsOccupied()) return;
+	void SetState(CellState state) override;
 
-		direction == Vertical ? this->state = VerticallyOccupied : this->state = HorizontallyOccupied;
-		vehicle = vehicleQueue.front();
-		vehicleQueue.pop();
-	}
+	void AddVehicle() override;
 };
