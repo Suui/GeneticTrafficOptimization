97c684a2d68e09200d3de242c279e221e9b5ca15
Suui <suui.gd@gmail.com>
2015-10-31 02:00:02 +0100

    Implemented: getters

--- Source/ParetoData/Vehicle.h
+++ Source/ParetoData/Vehicle.h
@@ -8,6 +8,7 @@ class Vehicle
 	std::vector<int> gasData;
 	const int id;
 
+
 public:
 
 	explicit Vehicle(int id) : id(id){}
@@ -15,4 +16,8 @@ public:
 	void AddMovement(int movement) { gasData.push_back(movement); }
 
 	void SetlastMovement(int movement) { gasData[gasData.size() - 1] = movement; }
+
+	int GetID() { return id; }
+
+	std::vector<int> GetGasData() { return gasData; }
 };
