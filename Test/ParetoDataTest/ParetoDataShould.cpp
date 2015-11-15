2eeee546df157abab1648162ba725192dffdda65
Suui <suui.gd@gmail.com>
2015-11-15 20:02:53 +0100

    Green: Return the correct average GHG

--- Test/ParetoDataTest/ParetoDataShould.cpp
+++ Test/ParetoDataTest/ParetoDataShould.cpp
@@ -8,5 +8,11 @@ TEST_CASE("ParetoData should")
 	{
 		ParetoDataStub paretoData;
 
+		std::map<int, std::vector<int>> data = { std::make_pair(0, std::vector<int> { 0, 1, 0, 0, 1, 1} ),
+												 std::make_pair(1, std::vector<int> { 1, 1, 0, 1, 0, 1} ) };
+												 
+		paretoData.StubSetGasData(data);
+
+		CHECK(paretoData.GetGHGAverage() == 2);
 	}
 }
